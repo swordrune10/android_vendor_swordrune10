1122db30b2c4f6774c7ff288b49f673a54073e8e
Sean Donovan <swordrune10@gmail.com>
2012-05-19 00:58:41 -0500

    back to hard links, cuz my computer hates me..
    i really cant wait to wipe this thing... wait.. i can do that tonight! =D
    Signed-off-by: Sean Donovan <swordrune10@gmail.com>

--- hijacker.sh
+++ hijacker.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd $REPOS/$SCRIPT_REPO-ics/vendor/swordrune10
-ln -s overrider.mk $REPOS/$SCRIPT_REPO-ics/device/samsung/toro/overrider.mk
-ln -s overrider.mk $REPOS/$SCRIPT_REPO-ics/device/samsung/maguro/overrider.mk
+ln overrider.mk $REPOS/$SCRIPT_REPO-ics/device/samsung/toro/overrider.mk
+ln overrider.mk $REPOS/$SCRIPT_REPO-ics/device/samsung/maguro/overrider.mk
