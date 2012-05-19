d37f301de847931d121da5c4a5d661672eb01eb9
Sean Donovan <swordrune10@gmail.com>
2012-05-19 00:55:52 -0500

    made symbolic linkage more specific
    Signed-off-by: Sean Donovan <swordrune10@gmail.com>

--- hijacker.sh
+++ hijacker.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 cd $REPOS/$SCRIPT_REPO-ics/vendor/swordrune10
-ln -s overrider.mk $REPOS/$SCRIPT_REPO-ics/device/*/*/overrider.mk
+ln -s overrider.mk $REPOS/$SCRIPT_REPO-ics/device/samsung/toro/overrider.mk
+ln -s overrider.mk $REPOS/$SCRIPT_REPO-ics/device/samsung/maguro/overrider.mk
