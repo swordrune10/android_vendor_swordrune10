78a85fb93a406c8d095e0974005b4307b9e9dd5c
Sean Donovan <swordrune10@gmail.com>
2012-05-18 20:35:39 -0500

    Initial commit, from the corpse of vendor_swordrune10
    tried to rename file and didnt work, wanted to change the name anyways..
    
    Signed-off-by: Sean Donovan <swordrune10@gmail.com>

---
+++ AndroidProducts.mk
@@ -0,0 +1,12 @@
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.rommanager.developerid=swordrune10 \
+    ro.config.ringtone=Ring_Synth_04.ogg \
+    ro.config.notification_sound=pixiedust.ogg \
+    ro.config.alarm_alert=Alarm_Classic.ogg \
+    ro.com.android.dataroaming=false
+
+PRODUCT_PACKAGES += \
+    SpareParts \
+    Superuser \
+    Superuser.apk \
+    su
