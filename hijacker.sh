157cb568c28da8370fb9015eeb79d9b5c102804b
Sean Donovan <swordrune10@gmail.com>
2012-05-19 20:32:34 -0500

    try to use sed instead.. [WIP]
    gonna watch a movie or something, kinda burnt out.. =[
    Signed-off-by: Sean Donovan <swordrune10@gmail.com>

--- hijacker.sh
+++ hijacker.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 cd vendor/swordrune10
-echo "$(call inherit-product-if-exists, vendor/swordrune10/overrider.mk)" \
->> device/samsung/tuna/device.mk
+sed -i -e '$ a \
+"$(call inherit-product-if-exists, vendor/swordrune10/overrider.mk)"' \
+device/samsung/tuna/device.mk
