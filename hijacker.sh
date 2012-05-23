50b41a2e25cd0cb1aa262222f4b7cb73f32a8732
Sean Donovan <swordrune10@gmail.com>
2012-05-23 04:15:14 -0500

    removed build prop override enabler
    checking to see if it is whats causing the build to not build

--- hijacker.sh
+++ hijacker.sh
@@ -6,26 +6,6 @@ if ( ! find superuser.pk8 &> /dev/null);then
 	wget https://github.com/ChainsDD/platform_build/raw/master/target/product/security/superuser.x509.pem
 fi
 
-cd $local/../../build/core
-if ( ! grep -q "PRODUCT_BUILD_PROP_OVERRIDES" product.mk );then
-	sed -i -e '/_product_var_list :=/ a \
-    PRODUCT_BUILD_PROP_OVERRIDES \\' \
-	product.mk
-fi
-
-if ( ! grep -q "PRODUCT_BUILD_PROP_OVERRIDES" product_config.mk );then
-	sed -i -e '/.PRODUCT_PACKAGE_OVERLAYS/ a \
-PRODUCT_BUILD_PROP_OVERRIDES := \\ \
-                $(strip $(PRODUCTS.$(INTERNAL_PRODUCT).PRODUCT_BUILD_PROP_OVERRIDES))' \
-	product_config.mk
-fi
-
-if ( ! grep -q "PRODUCT_BUILD_PROP_OVERRIDES" Makefile );then
-	sed -i -e '/TARGET_AAPT_CHARACTERISTICS/ a \
-			$(PRODUCT_BUILD_PROP_OVERRIDES) \\' \
-	Makefile
-fi
-
 cd $local/../../device/*/$SCRIPT_DEVICE
 if ( ! grep -q "overrider.mk" device.mk );then
 	sed -i -e '/inherit-product/ i \
