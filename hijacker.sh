e5e29ad2db46c1efcec77be87cbcfc841fd783f4
Sean Donovan <swordrune10@gmail.com>
2012-05-22 21:01:49 -0500

    make append higher, hopefully making it happen

--- hijacker.sh
+++ hijacker.sh
@@ -3,7 +3,7 @@ local="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $local/../../device/samsung/tuna
 if ( ! grep -q "overrider.mk" device.mk );then
 	cd $local
-	sed -i -e '$ a \
+	sed -i -e '/phone-xhdpi-1024-dalvik-heap.mk/ a \
 $(call inherit-product, vendor/swordrune10/overrider.mk)' \
 	../../device/samsung/tuna/device.mk
 fi
