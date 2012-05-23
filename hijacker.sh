1a22fd0cf5bd99bdc4854cf0ac5f8e4d7bc38cc2
Sean Donovan <swordrune10@gmail.com>
2012-05-22 21:29:27 -0500

    add superuser keys via wget

--- hijacker.sh
+++ hijacker.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 local="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd $local/../../build/target/product/security
+if ( ! find superuser.pk8 &> /dev/null);then
+	wget https://github.com/ChainsDD/platform_build/raw/master/target/product/security/superuser.pk8
+	wget https://github.com/ChainsDD/platform_build/raw/master/target/product/security/superuser.x509.pem
+fi
 cd $local/../../device/samsung/tuna
 if ( ! grep -q "overrider.mk" device.mk );then
 	cd $local
