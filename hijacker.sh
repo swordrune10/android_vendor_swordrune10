92a0be9f706b24c72db3298f1991518a14a2748f
Sean Donovan <swordrune10@gmail.com>
2012-05-22 20:42:54 -0500

    fix hijacker to only edit if it cant find it

--- hijacker.sh
+++ hijacker.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 local="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-cd $local
-sed -i -e '$ a \
-"$(call inherit-product-if-exists, vendor/swordrune10/overrider.mk)"' \
-../../device/samsung/tuna/device.mk
+cd $local/../../device/samsung/tuna
+if ( ! grep -q "overrider.mk" device.mk );then
+	cd $local
+	sed -i -e '$ a \
+	$(call inherit-product-if-exists, vendor/swordrune10/overrider.mk)' \
+	../../device/samsung/tuna/device.mk
+fi
