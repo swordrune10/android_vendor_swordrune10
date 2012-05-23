868b38f7db331a84427faa4ef7b135d2d5a827c5
Sean Donovan <swordrune10@gmail.com>
2012-05-23 00:33:16 -0500

    cleanup

--- overrider.mk
+++ overrider.mk
@@ -1,12 +1,7 @@
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.rommanager.developerid=$(shell whoami) \
-    ro.config.ringtone=Ring_Synth_04.ogg \
-    ro.config.notification_sound=pixiedust.ogg \
-    ro.config.alarm_alert=Alarm_Classic.ogg \
-    ro.com.android.dataroaming=false
+    ro.rommanager.developerid=$(shell whoami)
 
 PRODUCT_PACKAGES += \
-    SpareParts \
     Superuser \
     Superuser.apk \
     su
