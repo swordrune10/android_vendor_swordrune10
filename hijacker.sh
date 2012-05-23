e75b5ddfcc2b9ad84e0ec9593ed594477c3301c1
Sean Donovan <swordrune10@gmail.com>
2012-05-22 20:52:24 -0500

    change if exists to mandatory!

--- hijacker.sh
+++ hijacker.sh
@@ -4,6 +4,6 @@ cd $local/../../device/samsung/tuna
 if ( ! grep -q "overrider.mk" device.mk );then
 	cd $local
 	sed -i -e '$ a \
-$(call inherit-product-if-exists, vendor/swordrune10/overrider.mk)' \
+$(call inherit-product, vendor/swordrune10/overrider.mk)' \
 	../../device/samsung/tuna/device.mk
 fi
