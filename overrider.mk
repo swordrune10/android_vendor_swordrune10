f728807aff56d451d1bc7f74ce61d7a15e71db2e
Sean Donovan <swordrune10@gmail.com>
2012-05-23 23:39:36 -0500

    checking somethin..

--- overrider.mk
+++ overrider.mk
@@ -1,5 +1,7 @@
 PRODUCT_PROPERTY_OVERRIDES := \
-    ro.rommanager.developerid=$(shell whoami) \
+    ro.rommanager.developerid=$(shell whoami)
+
+PRODUCT_PROPERTY_OVERRIDES += \
     keyguard.no_require_sim=true \
     ro.com.android.dataroaming=false
 
