e1752edcf5a4b63335504fece41571ac2d2eeac2
Sean Donovan <swordrune10@gmail.com>
2012-05-23 23:30:30 -0500

    edit to work properly

--- scripts/hijacker.sh
+++ scripts/hijacker.sh
@@ -7,8 +7,7 @@ fi
 
 cd $ANDROID_BUILD_TOP/device/*/$SCRIPT_DEVICE
 if ( ! grep -q "overrider.mk" full_$SCRIPT_DEVICE.mk );then
-	sed -i -e '/ \
-.*inherit-product/ i \
+	sed -i -e '/full_base.*.mk/ i \
 $(call inherit-product, vendor/swordrune10/overrider.mk)' \
 	full_$SCRIPT_DEVICE.mk
 fi
