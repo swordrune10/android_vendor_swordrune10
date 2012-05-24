581a4e5ad114dcddee3f51b4a76baef7a86e67c1
Sean Donovan <swordrune10@gmail.com>
2012-05-24 01:00:46 -0500

    add sounds overrides

--- overrider.mk
+++ overrider.mk
@@ -3,7 +3,16 @@ PRODUCT_PROPERTY_OVERRIDES := \
 
 PRODUCT_PROPERTY_OVERRIDES += \
     keyguard.no_require_sim=true \
-    ro.com.android.dataroaming=false
+    ro.com.android.dataroaming=false \
+    ro.com.android.dateformat=MM-dd-yyyy \
+    ro.config.ringtone=CASSIOPEIA.ogg \
+    ro.config.notification_sound=Fluorine.ogg \
+    ro.config.alarm_alert=Hassium.ogg
+
+SOUNDS_PATH:= frameworks/base/data/sounds
+
+PRODUCT_COPY_FILES += \
+	$(SOUNDS_PATH)/alarms/ogg/Hassium.ogg:system/media/audio/alarms/Hassium.ogg
 
 PRODUCT_PACKAGES += \
     Superuser \
