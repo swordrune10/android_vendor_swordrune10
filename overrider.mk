7a0941d5cc4d5b5e4bdbb60ddbf948457f37104a
Sean Donovan <swordrune10@gmail.com>
2012-05-26 17:50:31 -0500

    change modversion so it would be easier to change

--- overrider.mk
+++ overrider.mk
@@ -1,11 +1,13 @@
 PRODUCT_PROPERTY_OVERRIDES := \
     ro.rommanager.developerid=$(shell whoami)
 
+MOD_VERSION := $SCRIPT_REPO-ics-$(date +%Y%m%d)-$SCRIPT_DEVICE
+
 PRODUCT_PROPERTY_OVERRIDES += \
     keyguard.no_require_sim=true \
     ro.com.android.dataroaming=false \
     ro.com.android.dateformat=MM-dd-yyyy \
-    ro.modversion=$SCRIPT_REPO-ics-$(date +%Y%m%d)-$SCRIPT_DEVICE \
+    ro.modversion=$(MOD_VERSION) \
     ro.config.ringtone=CASSIOPEIA.ogg \
     ro.config.notification_sound=Fluorine.ogg \
     ro.config.alarm_alert=Hassium.ogg
