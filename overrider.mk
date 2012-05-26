e652fe1912be7041da9e2fd860b9696f87b05e9d
Sean Donovan <swordrune10@gmail.com>
2012-05-25 19:01:32 -0500

    added modversion for rom manager

--- overrider.mk
+++ overrider.mk
@@ -5,6 +5,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     keyguard.no_require_sim=true \
     ro.com.android.dataroaming=false \
     ro.com.android.dateformat=MM-dd-yyyy \
+    ro.modversion=$SCRIPT_REPO-ics-$(date +%Y%m%d)-$SCRIPT_DEVICE \
     ro.config.ringtone=CASSIOPEIA.ogg \
     ro.config.notification_sound=Fluorine.ogg \
     ro.config.alarm_alert=Hassium.ogg
