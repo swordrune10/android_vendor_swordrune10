ca6dcee3b97fd901e457fc4c4c590030dbf389a3
Sean Donovan <swordrune10@gmail.com>
2012-05-28 20:44:01 -0500

    modify hijacker to be more independent.. last claim of independence
    vendor tree ready to be used anything in aosp

--- scripts/hijacker.sh
+++ scripts/hijacker.sh
@@ -5,9 +5,9 @@ if ( ! find superuser.pk8 &> /dev/null);then
 	wget https://github.com/ChainsDD/platform_build/raw/master/target/product/security/superuser.x509.pem
 fi
 
-cd $ANDROID_BUILD_TOP/device/*/$SCRIPT_DEVICE
-if ( ! grep -q "overrider.mk" full_$SCRIPT_DEVICE.mk );then
+cd $ANDROID_BUILD_TOP/device/*/$TARGET_PRODUCT
+if ( ! grep -q "overrider.mk" full_$TARGET_PRODUCT.mk );then
 	sed -i -e '/full_base.*.mk/ i \
 $(call inherit-product, vendor/swordrune10/overrider.mk)' \
-	full_$SCRIPT_DEVICE.mk
+	full_$TARGET_PRODUCT.mk
 fi
