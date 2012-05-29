ef64934fdc2059a220ef06905a9f8d0b3f194f4e
Sean Donovan <swordrune10@gmail.com>
2012-05-28 23:03:35 -0500

    this is starting to not be funny..

--- scripts/hijacker.sh
+++ scripts/hijacker.sh
@@ -10,6 +10,6 @@ AOSP_BUILD=$(echo $TARGET_PRODUCT | sed -e 's/^full_//g')
 cd $ANDROID_BUILD_TOP/device/*/$AOSP_BUILD
 if ( ! grep -q "overrider.mk" $TARGET_PRODUCT.mk );then
 	sed -i -e '/full_base.*.mk/ i \
-$(call inherit-product, vendor/swordrune10/makefiles/overrider.mk)' \
+$(call inherit-product, $(ANDROID_BUILD_TOP)/vendor/swordrune10/makefiles/overrider.mk)' \
 	$TARGET_PRODUCT.mk
 fi
