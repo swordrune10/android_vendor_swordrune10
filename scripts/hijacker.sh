de9bc39facef63236458bd1bbe6d0915bd37e6e5
Sean Donovan <swordrune10@gmail.com>
2012-06-04 08:59:01 -0500

    export AOSP_BUILD for modversion/remove absolute path for overrider.mk

--- scripts/hijacker.sh
+++ scripts/hijacker.sh
@@ -5,11 +5,11 @@ if ( ! find superuser.pk8 &> /dev/null);then
 	wget https://github.com/ChainsDD/platform_build/raw/master/target/product/security/superuser.x509.pem
 fi
 
-AOSP_BUILD=$(echo $TARGET_PRODUCT | sed -e 's/^full_//g')
+export AOSP_BUILD=$(echo $TARGET_PRODUCT | sed -e 's/^full_//g')
 
 cd $ANDROID_BUILD_TOP/device/*/$AOSP_BUILD
 if ( ! grep -q "overrider.mk" $TARGET_PRODUCT.mk );then
 	sed -i -e '/full_base.*.mk/ i \
-$(call inherit-product, $(ANDROID_BUILD_TOP)/vendor/swordrune10/makefiles/overrider.mk)' \
+$(call inherit-product, vendor/swordrune10/makefiles/overrider.mk)' \
 	$TARGET_PRODUCT.mk
 fi
